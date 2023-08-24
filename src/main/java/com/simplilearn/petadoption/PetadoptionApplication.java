3a15cce8a259da282a028e636cb6fec3587c9647
Thri2108 <tyerabolu@gmail.com>
2023-08-24 19:40:34 +0000

    first commit

--- src/main/java/com/simplilearn/petadoption/PetadoptionApplication.java
+++ src/main/java/com/simplilearn/petadoption/PetadoptionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetadoptionApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.location", "application.properties");
 		SpringApplication.run(PetadoptionApplication.class, args);
 	}
 
--- src/main/java/com/simplilearn/petadoption/controllers/ApplicationController.java
+++ src/main/java/com/simplilearn/petadoption/controllers/ApplicationController.java
@@ -2,8 +2,9 @@ package com.simplilearn.petadoption.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ApplicationController {
     @RequestMapping("/")
     public String startApplication(){
